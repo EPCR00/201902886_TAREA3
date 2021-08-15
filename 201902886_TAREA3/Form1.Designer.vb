f3aa222a6742f6aed520a75f8b79d9a52b1ae0d8
EPCR00 <3339068101301@ingenieria.usac.edu.gt>
2021-08-15 04:52:44 -0600

    Initial commit

---
+++ 201902886_TAREA3/Form1.Designer.vb
@@ -0,0 +1,357 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.montoEF = New System.Windows.Forms.TextBox()
+        Me.montoCOB = New System.Windows.Forms.TextBox()
+        Me.montoCP = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.saldoAnterior = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Calcular = New System.Windows.Forms.Button()
+        Me.limpiar = New System.Windows.Forms.Button()
+        Me.cerrar = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.saldoNuevo = New System.Windows.Forms.Label()
+        Me.txtinteres = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox4.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.RadioButton3)
+        Me.GroupBox1.Controls.Add(Me.RadioButton2)
+        Me.GroupBox1.Controls.Add(Me.RadioButton1)
+        Me.GroupBox1.Location = New System.Drawing.Point(26, 22)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(157, 156)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Tipo de Depósito:"
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(25, 120)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(56, 17)
+        Me.RadioButton3.TabIndex = 2
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "Ahorro"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(25, 77)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(110, 17)
+        Me.RadioButton2.TabIndex = 1
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Monetario Premier"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(25, 38)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(72, 17)
+        Me.RadioButton1.TabIndex = 0
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Monetario"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.CheckBox3)
+        Me.GroupBox2.Controls.Add(Me.CheckBox2)
+        Me.GroupBox2.Controls.Add(Me.CheckBox1)
+        Me.GroupBox2.Location = New System.Drawing.Point(209, 22)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(168, 156)
+        Me.GroupBox2.TabIndex = 2
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Método de Depósito:"
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(25, 121)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(65, 17)
+        Me.CheckBox3.TabIndex = 2
+        Me.CheckBox3.Text = "Efectivo"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(25, 78)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(123, 17)
+        Me.CheckBox2.TabIndex = 1
+        Me.CheckBox2.Text = "Cheque Otro Banco "
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(25, 38)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(96, 17)
+        Me.CheckBox1.TabIndex = 0
+        Me.CheckBox1.Text = "Cheque Propio"
+        Me.CheckBox1.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.montoEF)
+        Me.GroupBox3.Controls.Add(Me.montoCOB)
+        Me.GroupBox3.Controls.Add(Me.montoCP)
+        Me.GroupBox3.Controls.Add(Me.Label3)
+        Me.GroupBox3.Controls.Add(Me.Label2)
+        Me.GroupBox3.Controls.Add(Me.Label1)
+        Me.GroupBox3.Location = New System.Drawing.Point(404, 23)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(172, 155)
+        Me.GroupBox3.TabIndex = 3
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Monto"
+        '
+        'montoEF
+        '
+        Me.montoEF.Location = New System.Drawing.Point(43, 113)
+        Me.montoEF.Name = "montoEF"
+        Me.montoEF.Size = New System.Drawing.Size(100, 20)
+        Me.montoEF.TabIndex = 5
+        Me.montoEF.Text = "0"
+        '
+        'montoCOB
+        '
+        Me.montoCOB.Location = New System.Drawing.Point(43, 75)
+        Me.montoCOB.Name = "montoCOB"
+        Me.montoCOB.Size = New System.Drawing.Size(100, 20)
+        Me.montoCOB.TabIndex = 4
+        Me.montoCOB.Text = "0"
+        '
+        'montoCP
+        '
+        Me.montoCP.Location = New System.Drawing.Point(43, 35)
+        Me.montoCP.Name = "montoCP"
+        Me.montoCP.Size = New System.Drawing.Size(100, 20)
+        Me.montoCP.TabIndex = 3
+        Me.montoCP.Text = "0"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(22, 120)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(15, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Q"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(22, 77)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(15, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Q"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(22, 38)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(15, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Q"
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.saldoAnterior)
+        Me.GroupBox4.Controls.Add(Me.Label4)
+        Me.GroupBox4.Location = New System.Drawing.Point(26, 213)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(157, 75)
+        Me.GroupBox4.TabIndex = 4
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "Saldo Anterior"
+        '
+        'saldoAnterior
+        '
+        Me.saldoAnterior.Location = New System.Drawing.Point(43, 37)
+        Me.saldoAnterior.Name = "saldoAnterior"
+        Me.saldoAnterior.Size = New System.Drawing.Size(100, 20)
+        Me.saldoAnterior.TabIndex = 1
+        Me.saldoAnterior.Text = "0"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(22, 39)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(15, 13)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "Q"
+        '
+        'Calcular
+        '
+        Me.Calcular.Location = New System.Drawing.Point(612, 38)
+        Me.Calcular.Name = "Calcular"
+        Me.Calcular.Size = New System.Drawing.Size(75, 23)
+        Me.Calcular.TabIndex = 5
+        Me.Calcular.Text = "&Calcular"
+        Me.Calcular.UseVisualStyleBackColor = True
+        '
+        'limpiar
+        '
+        Me.limpiar.Location = New System.Drawing.Point(612, 90)
+        Me.limpiar.Name = "limpiar"
+        Me.limpiar.Size = New System.Drawing.Size(75, 23)
+        Me.limpiar.TabIndex = 10
+        Me.limpiar.Text = "&Limpiar"
+        Me.limpiar.UseVisualStyleBackColor = True
+        '
+        'cerrar
+        '
+        Me.cerrar.Location = New System.Drawing.Point(612, 139)
+        Me.cerrar.Name = "cerrar"
+        Me.cerrar.Size = New System.Drawing.Size(75, 23)
+        Me.cerrar.TabIndex = 11
+        Me.cerrar.Text = "&Cerrar"
+        Me.cerrar.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(335, 222)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(72, 13)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "Saldo Nuevo:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(335, 257)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(42, 13)
+        Me.Label6.TabIndex = 13
+        Me.Label6.Text = "Interés:"
+        '
+        'saldoNuevo
+        '
+        Me.saldoNuevo.AutoSize = True
+        Me.saldoNuevo.Location = New System.Drawing.Point(413, 222)
+        Me.saldoNuevo.Name = "saldoNuevo"
+        Me.saldoNuevo.Size = New System.Drawing.Size(13, 13)
+        Me.saldoNuevo.TabIndex = 14
+        Me.saldoNuevo.Text = "0"
+        '
+        'txtinteres
+        '
+        Me.txtinteres.AutoSize = True
+        Me.txtinteres.Location = New System.Drawing.Point(383, 257)
+        Me.txtinteres.Name = "txtinteres"
+        Me.txtinteres.Size = New System.Drawing.Size(13, 13)
+        Me.txtinteres.TabIndex = 15
+        Me.txtinteres.Text = "0"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(718, 320)
+        Me.Controls.Add(Me.txtinteres)
+        Me.Controls.Add(Me.saldoNuevo)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.cerrar)
+        Me.Controls.Add(Me.limpiar)
+        Me.Controls.Add(Me.Calcular)
+        Me.Controls.Add(Me.GroupBox4)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.GroupBox4.ResumeLayout(False)
+        Me.GroupBox4.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents montoEF As TextBox
+    Friend WithEvents montoCOB As TextBox
+    Friend WithEvents montoCP As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox4 As GroupBox
+    Friend WithEvents saldoAnterior As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Calcular As Button
+    Friend WithEvents limpiar As Button
+    Friend WithEvents cerrar As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents saldoNuevo As Label
+    Friend WithEvents txtinteres As Label
+End Class
---
+++ 201902886_TAREA3/Form1.vb
@@ -0,0 +1,117 @@
+﻿Public Class Form1
+
+    Private Sub Calcular_Click(sender As Object, e As EventArgs) Handles Calcular.Click
+
+        If RadioButton3.Checked = True Then
+            Select Case saldo_nuevo
+                Case 1.01 To 1000.99
+                    interes = 1.5 / 100
+                Case 1001 To 5000.99
+                    interes = 2.5 / 100
+                Case 5001 To 15000.99
+                    interes = 7.5 / 100
+                Case Is >= 15001
+                    interes = 10 / 100
+            End Select
+            saldo_nuevo = Val(montoCP.Text) + Val(montoCOB.Text) + Val(montoEF.Text) + Val(saldoAnterior.Text)
+            saldo_nuevo = saldo_nuevo + saldo_nuevo * interes
+            saldoNuevo.Text = Str(saldo_nuevo)
+        End If
+
+        If RadioButton2.Checked = True Then
+            Select Case Val(saldoAnterior.Text)
+                Case 1.01 To 10000.99
+                    interes = 2.5 / 100
+                Case 10001 To 15000.99
+                    interes = 3 / 100
+                Case Is >= 15001
+                    interes = 4.5 / 100
+            End Select
+            saldo_nuevo = Val(montoCP.Text) + Val(montoCOB.Text) + Val(montoEF.Text) + Val(saldoAnterior.Text) * interes
+            saldoNuevo.Text = Str(saldo_nuevo)
+        End If
+
+        If RadioButton1.Checked = True Then
+            saldo_nuevo = Val(montoCP.Text) + Val(montoCOB.Text) + Val(montoEF.Text) + Val(saldoAnterior.Text)
+            saldoNuevo.Text = Str(saldo_nuevo)
+        End If
+
+        txtinteres.Text = Str(interes * 100) + "%"
+
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If RadioButton1.Checked = True Then
+            tipoSeleccionado = True
+        ElseIf RadioButton2.Checked = True Then
+            tipoSeleccionado = True
+        ElseIf RadioButton3.Checked = True Then
+            tipoSeleccionado = True
+        End If
+
+        If tipoSeleccionado = True Then
+            If CheckBox1.Checked = True Then
+                montoCP.Enabled = True
+            End If
+
+            If CheckBox1.Checked = False Then
+                montoCP.Enabled = False
+            End If
+        End If
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+        If RadioButton1.Checked = True Then
+            tipoSeleccionado = True
+        ElseIf RadioButton2.Checked = True Then
+            tipoSeleccionado = True
+        ElseIf RadioButton3.Checked = True Then
+            tipoSeleccionado = True
+        End If
+
+        If tipoSeleccionado = True Then
+            If CheckBox2.Checked = True Then
+                montoCOB.Enabled = True
+            End If
+
+            If CheckBox2.Checked = False Then
+                montoCOB.Enabled = False
+            End If
+        End If
+    End Sub
+
+    Private Sub CheckBox3_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox3.CheckedChanged
+        If RadioButton1.Checked = True Then
+            tipoSeleccionado = True
+        ElseIf RadioButton2.Checked = True Then
+            tipoSeleccionado = True
+        ElseIf RadioButton3.Checked = True Then
+            tipoSeleccionado = True
+        End If
+
+        If tipoSeleccionado = True Then
+            If CheckBox3.Checked = True Then
+                montoEF.Enabled = True
+            End If
+
+            If CheckBox3.Checked = False Then
+                montoEF.Enabled = False
+            End If
+        End If
+    End Sub
+
+    Private Sub limpiar_Click(sender As Object, e As EventArgs) Handles limpiar.Click
+        montoCP.Clear()
+        montoCOB.Clear()
+        montoEF.Clear()
+        saldoAnterior.Clear()
+
+    End Sub
+
+    Private Sub cerrar_Click(sender As Object, e As EventArgs) Handles cerrar.Click
+        If MsgBox("¿Desea salir?", vbQuestion + vbYesNo, "Pregunta") = vbYes Then
+            End
+        End If
+    End Sub
+
+End Class
